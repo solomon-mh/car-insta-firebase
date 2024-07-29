d1a158d1b0f6926a99b80decb24d0c309dc756f9
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-29 14:10:56 +0300

    style: modfying the the ui

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,6 +1,5 @@
 import { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
-
 import { Button } from "../ui/button";
 import { convertFileToUrl } from "@/lib/utils";
 
@@ -10,16 +9,13 @@ type FileUploaderProps = {
 };
 
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
-  const [file, setFile] = useState<File[]>([]);
   const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
-
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
-      setFile(acceptedFiles);
       fieldChange(acceptedFiles);
       setFileUrl(convertFileToUrl(acceptedFiles[0]));
     },
-    [file]
+    [fieldChange]
   );
 
   const { getRootProps, getInputProps } = useDropzone({
@@ -32,32 +28,40 @@ const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
   return (
     <div
       {...getRootProps()}
-      className='flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer'
+      className="flex flex-col max-h-96 bg-dark-3 rounded-xl cursor-pointer max-w-sm"
     >
-      <input {...getInputProps()} className='cursor-pointer' />
+      <input {...getInputProps()} className="cursor-pointer" />
 
       {fileUrl ? (
         <>
-          <div className='flex flex-1 justify-center w-full p-5 lg:p-10'>
-            <img src={fileUrl} alt='image' className='file_uploader-img' />
+          <div className="flex flex-1 justify-center w-full p-5 lg:p-10">
+            <img
+              src={fileUrl}
+              alt="image"
+              className="file_uploader-img max-h-60"
+            />
           </div>
-          <p className='file_uploader-label'>Click or drag photo to replace</p>
+          <p className="file_uploader-label text-center">
+            Click or drag photo to replace
+          </p>
         </>
       ) : (
-        <div className='file_uploader-box '>
+        <div className="file_uploader-box flex flex-col items-center p-5">
           <img
-            src='/assets/icons/file-upload.svg'
-            width={96}
-            height={77}
-            alt='file upload'
+            src="/assets/icons/file-upload.svg"
+            width={48}
+            height={38.5}
+            alt="file upload"
           />
 
-          <h3 className='base-medium text-light-2 mb-2 mt-6'>
+          <h3 className="base-medium text-light-2 mb-2 mt-6 text-center">
             Drag photo here
           </h3>
-          <p className='text-light-4 small-regular mb-6'>SVG, PNG, JPG</p>
+          <p className="text-light-4 small-regular mb-6 text-center">
+            SVG, PNG, JPG
+          </p>
 
-          <Button type='button' className='shad-button_dark_4'>
+          <Button type="button" className="shad-button_dark_4">
             Select from computer
           </Button>
         </div>
