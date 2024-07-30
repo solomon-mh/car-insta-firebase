f6add2ab7a45eaa639dacc23af3c89611d5a6b0c
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-07-30 14:12:05 +0300

    feat: adding getCurrentUser functionality

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,43 +1,54 @@
-import { useUserContext } from "@/context/AuthContext";
 import { Link, NavLink, useLocation, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
-import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
-import { useEffect } from "react";
 import { sidebarLinks } from "@/constants";
-import { INavLink } from "@/types";
+import { INavLink, IUser } from "@/types";
+import { getCurrentUser, signOutUser } from "@/lib/firebase/api";
+import { useEffect, useState } from "react";
 
 const LeftSidebar = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
-  const { user } = useUserContext();
-  const { mutate: signOut, isSuccess } = useSignOutAccount();
-  useEffect(() => {
-    if (isSuccess) navigate(0);
-  }, [isSuccess, navigate]);
+  function handleSignOut() {
+    signOutUser();
+    navigate("/sign-in");
+  }
+  const [user, setUser] = useState<IUser | null>(null);
+  // console.log(user);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const user = await getCurrentUser();
+        setUser(user as IUser | null);
+      } catch (error) {
+        console.log("Error Fetching User", error);
+      }
+    };
+    fetchUser();
+  }, []);
   return (
-    <nav className='leftsidebar'>
-      <div className='flex flex-col gap-11'>
-        <Link to='/' className='flex gap-3 items-center'>
+    <nav className="leftsidebar">
+      <div className="flex flex-col gap-11">
+        <Link to="/" className="flex gap-3 items-center">
           <img
-            src='/assets/images/logo.svg'
-            alt='logo'
+            src="/assets/images/logo.svg"
+            alt="logo"
             width={170}
             height={36}
           />
         </Link>
-        <Link to={`/profile/${user.id}`} className='flex items-center gap-3'>
+        <Link to={`/profile/`} className="flex items-center gap-3">
           <img
-            src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
-            alt='profile'
-            className='h-14 w-14 rounded-full'
+            src={"/assets/icons/profile-placeholder.svg"}
+            alt="profile"
+            className="h-14 w-14 rounded-full"
           />
-          <div className='flex flex-col'>
-            <p className='body-bold'>{user.name}</p>
-            <p className='small-regular text-light-3'>@{user.username}</p>
+          <div className="flex flex-col">
+            <p className="body-bold">{user?.name}</p>
+            <p className="small-regular text-light-3">@{user?.username}</p>
           </div>
         </Link>
-        <ul className='flex flex-col gap-1'>
+        <ul className="flex flex-col gap-1">
           {sidebarLinks.map((link: INavLink) => {
             const isActive = pathname === link.route;
             return (
@@ -49,7 +60,7 @@ const LeftSidebar = () => {
               >
                 <NavLink
                   to={link.route}
-                  className='flex gap-4 items-center p-3'
+                  className="flex gap-4 items-center p-3"
                 >
                   <img
                     src={link.imgURL}
@@ -66,12 +77,12 @@ const LeftSidebar = () => {
         </ul>
       </div>
       <Button
-        variant='ghost'
-        className='shad-button_ghost'
-        onClick={() => signOut()}
+        onClick={() => handleSignOut()}
+        variant="ghost"
+        className="shad-button_ghost"
       >
-        <img src='/assets/icons/logout.svg' alt='logout' />
-        <p title='' className='small-medium lg:base-medium'>
+        <img src="/assets/icons/logout.svg" alt="logout" />
+        <p title="" className="small-medium lg:base-medium">
           LogOut
         </p>
       </Button>
